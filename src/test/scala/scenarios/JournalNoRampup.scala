01791d8c36e8e854a7dc83a37875b9b91c1c6c73
Teodor Turonek <T.Turonek@kainos.com>
2019-02-22 14:53:31 +0100

    report folder renamed check 200 changed to 403

--- src/test/scala/scenarios/JournalNoRampup.scala
+++ src/test/scala/scenarios/JournalNoRampup.scala
@@ -16,7 +16,7 @@ class JournalNoRampup extends Simulation {
   val csvFeeder = csv("users.csv").circular
 
   //setting authorisation token --Temporary Solution--
-  private val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1zeE1KTUxDSURXTVRQdlp5SjZ0eC1DRHh3MCIsImtpZCI6Ii1zeE1KTUxDSURXTVRQdlp5SjZ0eC1DRHh3MCJ9.eyJhdWQiOiIwOWZkZDY4Yy00ZjJmLTQ1YzItYmU1NS1kZDk4MTA0ZDRmNzQiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC82YzQ0OGQ5MC00Y2ExLTRjYWYtYWI1OS0wYTJhYTY3ZDc4MDEvIiwiaWF0IjoxNTUwODM3NTM5LCJuYmYiOjE1NTA4Mzc1MzksImV4cCI6MTU1MDgzOTAzOSwiYWNyIjoiMSIsImFpbyI6IjQySmdZUENxeURnbU1vRnhXV2JoL25XcnZmUkRyOVl1amZ2a2YxMXRqMUVuUzQzTjIzQUEiLCJhbXIiOlsicHdkIl0sImFwcGlkIjoiMDlmZGQ2OGMtNGYyZi00NWMyLWJlNTUtZGQ5ODEwNGQ0Zjc0IiwiYXBwaWRhY3IiOiIwIiwiZXh0bi5lbXBsb3llZUlkIjpbIjAxMjM0NTY3Il0sImlwYWRkciI6IjkxLjIyMi43MS45OCIsIm5hbWUiOiJNRVNCZXRhIFVzZXIgMSIsIm9pZCI6IjhlMDY0NjgwLThhYmUtNGVmOS1hY2JiLTI5ZDVhMzBhYTZlYyIsInNjcCI6IkRpcmVjdG9yeS5SZWFkLkFsbCBVc2VyLlJlYWQiLCJzdWIiOiJ2cEp3RFVvTG1pcEtETTB4TDU3ODBhazV4M2taWS1DOWlkT3RYbWNNUVVJIiwidGlkIjoiNmM0NDhkOTAtNGNhMS00Y2FmLWFiNTktMGEyYWE2N2Q3ODAxIiwidW5pcXVlX25hbWUiOiJtb2JleGFtaW5lcjFAZHZzYWdvdi5vbm1pY3Jvc29mdC5jb20iLCJ1cG4iOiJtb2JleGFtaW5lcjFAZHZzYWdvdi5vbm1pY3Jvc29mdC5jb20iLCJ1dGkiOiJ2NDVheGR2N3NVcU44dEpsMGtsUUFBIiwidmVyIjoiMS4wIn0.GCHYGp7_W8MYx2hlOuuHrgRury68w__Y36Qw70oMruaJeEwc_Ub4BFaBGOfcsjfY3mQkBcmvnct8a2MTb1Xgid0A2yJ-dwMe7ljrTyb9ydlHRMxHcfDntAiCDlNFAnrlKHKdQidGf8vYopV28kbLwMilH6mbiwfdh9QFtmYYFggPqnwCYL2ENoTzyPC2FWMaNk1Hejr0iqmgjfW1FMQ4TVUapRlvuxKy-hJwkh4pCeffWYbqru1Mi1frr-C-JyvY-pdQZcijk4aQrCGx7bSfuuhkBEMug7woLeeUVXAi4J6XTW6zDBE2T6fZrQ2MhUINpzlvL2kjBueBSL5ejILt3A"
+  private val token = "token"
   val headers_10 = Map("Content-Type" -> """application/json""", "Authorization" -> token)
 
   //values for scenario
@@ -41,7 +41,7 @@ class JournalNoRampup extends Simulation {
     .exec(http("Get_Journal")
       .get(uri)
       .headers(headers_10)
-      .check(status.is(200)))
+      .check(status.is(403)))
     .pause(Duration.apply(waitTime, TimeUnit.SECONDS))
 
   val scn2: ScenarioBuilder = scenario("Get_Journal2")
@@ -49,7 +49,7 @@ class JournalNoRampup extends Simulation {
     .exec(http("Error_CSV")
       .get("${user}")
       .headers(headers_10)
-      .check(status.is(200)))
+      .check(status.is(403)))
     .pause(Duration.apply(waitTime, TimeUnit.SECONDS))
 
   //Setup for users and maximum run time values
