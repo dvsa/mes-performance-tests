f68459b647b4bce222e07673583b58862f2eac75
Teodor Turonek <T.Turonek@kainos.com>
2019-02-25 15:53:31 +0000

    target folder removed

--- src/test/scala/scenarios/SingleGetJournal.scala
+++ src/test/scala/scenarios/SingleGetJournal.scala
@@ -17,7 +17,7 @@ class SingleGetJournal extends Simulation {
   val csvuser = csv("users.csv")
 
   //values for scenario
-  private val baseUrl = "https://teo.mes.dev-dvsacloud.uk/v1/journals/"
+  private val baseUrl = System.getenv("BASE_URL")
   private val uri2 = baseUrl + "67128492/personal"
   private val contentType = "application/json"
 
