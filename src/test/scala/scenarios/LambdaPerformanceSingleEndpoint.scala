30e763353f592013c7333d3fb46714b090cbb896
Teodor Turonek <T.Turonek@kainos.com>
2019-02-25 13:10:12 +0000

    no message

--- src/test/scala/scenarios/LambdaPerformanceSingleEndpoint.scala
+++ src/test/scala/scenarios/LambdaPerformanceSingleEndpoint.scala
@@ -12,25 +12,26 @@ import scala.concurrent.duration.{Duration, FiniteDuration}
 
 class LambdaPerformanceSingleEndpoint extends Simulation {
 
-//setting authorisation token --Temporary Solution--
+  //setting authorisation token --Temporary Solution--
   private val token = System.getenv("AD_JWT_TOKEN")
   val headers = Map("Content-Type" -> """application/json""", "Authorization" -> token)
 
-//values for scenario
-  private val baseUrl = System.getenv("BASE_URL")// "https://dev.mes.dev-dvsacloud.uk/v1/"
+  //values for scenario
+  private val baseUrl = System.getenv("BASE_URL")
+  // "https://dev.mes.dev-dvsacloud.uk/v1/"
   private val contentType = "application/json" // content type for httpProtocol
 
-// values for setUp phase
+  // values for setUp phase
   // csv feeder currently not working csv file stored in test/resources
   val csvUser = csv("users.csv").circular
   // wait before scenario ends (seconds)
   private val waitTime = 1
   // max users used at once in scenario
-  private val maxUsers = System.getenv("NO_USERS")
+  private val maxUsers = 10
   // time to ramp up users to full capacity (seconds)
-  private val rampUpDuration = System.getenv("RAMP_UP_TIME")
+  private val rampUpDuration = 10
   // duration of test run (seconds)
-  private val maxDuration = System.getenv("JOB_DURATION")
+  private val maxDuration = 180
 
   val httpProtocol: HttpProtocolBuilder = http
     .baseUrl(baseUrl)
@@ -38,31 +39,28 @@ class LambdaPerformanceSingleEndpoint extends Simulation {
     .acceptHeader("*/*")
     .contentTypeHeader(contentType)
 
-// executable scenario section
+  // executable scenario section
   val scn: ScenarioBuilder = scenario("Get_Journal")
-     // forever loop max duration can be found in setUp section
+    // forever loop max duration can be found in setUp section
     .forever("Get Journal", exitASAP = true) {
-     // loads values from csv
+    // loads values from csv
     feed(csvUser)
-      exec(http("Get_Journal_67128492")
-        // get on url with endpoint from feeder
-        .get(baseUrl + "${user}")
-        // sets headers
-        .headers(headers)
-        // checks status
-        .check(status.is(200),
-        // checks if response body contains "testSlot"
-        substring("testSlot")))
-        // wait before loop ends
-        .pause(Duration.apply(waitTime, TimeUnit.SECONDS))
-    }
+    exec(http("Get_" + "${user}")
+      // get on url with endpoint from feeder
+      .get(baseUrl + "${user}")
+      // sets headers
+      .headers(headers)
+      // checks status
+      .check(status.is(200),
+      // checks if response body contains "testSlot"
+      substring("testSlot")))
+      // wait before loop ends
+      .pause(Duration.apply(waitTime, TimeUnit.SECONDS))
+  }
 
-// setUp section allows to change ramp up and sets maximum duration of the test
+
+  // setUp section allows to change ramp up and sets maximum duration of the test
   // max number of users achieved in set amount of time then simulation runs on loop until maxDuration expires
-  setUp(
-    scn.
-      inject(
-        heavisideUsers(maxUsers) during rampUpDuration))
-    .maxDuration(maxDuration, TimeUnit.SECONDS)
-
+setUp(scn
+  .inject(atOnceUsers(maxUsers))).maxDuration(maxDuration)
 }
