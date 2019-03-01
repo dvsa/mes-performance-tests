08f2bee16dda1e1cfc478f4136bb4c8acf2742db
Teodor Turonek <T.Turonek@kainos.com>
2019-03-01 16:27:06 +0100

    logs microservice test1

--- src/test/scala/scenarios/loggingService.scala
+++ src/test/scala/scenarios/loggingService.scala
@@ -11,19 +11,18 @@ import io.gatling.http.protocol.HttpProtocolBuilder
 import scala.concurrent.duration.{Duration, FiniteDuration}
 
 
-class JournalNoRampup extends Simulation {
-
-  // csv feeder currently not working csv file stored in test/resources
-  val csvFeeder = csv("users.csv").circular
-  val headers_10 = Map("Content-Type" -> """application/json""", "Authorization" -> token)
+class loggingService extends Simulation {
 
   //setting authorisation token --Temporary Solution--
   private val token = System.getenv("AD_JWT_TOKEN")
 
+  // csv feeder currently not working csv file stored in test/resources
+  val errorLog = csv("errorLog.csv").circular
+  val headers_10 = Map("Content-Type" -> """application/json""", "Authorization" -> token)
+
   //values for scenario
   private val baseUrl = System.getenv("BASE_URL")
-  private val uri = baseUrl + "01234567/personal"
-  private val uri2 = baseUrl + "67128492/personal"
+  private val uri = baseUrl + "logs"
   private val contentType = "application/json"
   //values for setUp phase
   private val maxUsers = System.getenv("NO_USERS").toInt
@@ -35,32 +34,19 @@ class JournalNoRampup extends Simulation {
     .acceptHeader("*/*")
     .contentTypeHeader(contentType)
 
-  val scn: ScenarioBuilder = scenario("Get_Journal")
-  feed(csvFeeder)
-    .exec(http("Get_Journal")
-      .get(uri)
+  val scn: ScenarioBuilder = scenario("Send_Logs")
+  feed(errorLog)
+    .exec(http("Send_Logs")
+      .post(uri)
       .headers(headers_10)
-      .check(status.is(200),
-        substring("examinerName")))
+      .body("${logs}")
+      .check(status.is(200)))
     .pause(Duration.apply(waitTime, TimeUnit.SECONDS))
   private val maxDuration = System.getenv("JOB_DURATION").toInt
-
-  val scn2: ScenarioBuilder = scenario("Get_Journal2")
-  feed(csvFeeder)
-    .exec(http("Error_CSV")
-      .get(baseUrl + "${user}")
-      .headers(headers_10)
-      .check(status.is(200),
-        substring("examinerName")))
-    .pause(Duration.apply(waitTime, TimeUnit.SECONDS))
 
   //Setup for users and maximum run time values
   setUp(
     scn
-      .inject(
-        nothingFor(waitTime),
-        atOnceUsers(maxUsers)),
-    scn2
       .inject(
         nothingFor(waitTime),
         atOnceUsers(maxUsers)))
