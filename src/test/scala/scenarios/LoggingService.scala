ec3e42c304a84faead5c87b63d0f4bdc60deb845
Teodor Turonek <44288821+turonekt@users.noreply.github.com>
2019-06-12 14:56:26 +0100

    Update LoggingService.scala

--- src/test/scala/scenarios/LoggingService.scala
+++ src/test/scala/scenarios/LoggingService.scala
@@ -41,7 +41,7 @@ class LoggingService extends Simulation {
           exec(http("Send_Logs")
             .post(uri)
             .headers(headers_10)
-          .body(StringBody("[{
+          .body(StringBody([{
                                "type": "info",
                                "message": "DE with id: 47182032 - [JournalPage] Load Journal Test",
                                "timestamp": 1552994170000
@@ -61,7 +61,7 @@ class LoggingService extends Simulation {
                                "message": "DE with id: 47182032 - [JournalPage] Load Journal Test",
                                "timestamp": 1552994170000
                                }]
-                           "))
+                           ))
           .check(status.is(200),
             substring("received and saved.")))
       }
